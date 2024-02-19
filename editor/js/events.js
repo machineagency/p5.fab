edc2cdf69eb3dab04cac05c7fad779bd850fdc1b
Ray Song <raysong@rayx.local>
2024-02-19 06:32:55 -0800

    updated lib

--- editor/js/events.js
+++ editor/js/events.js
@@ -45,6 +45,7 @@ function saveSketch() {
   downloadLink.click();
 }
 
+// handle uploaded gcode file
 document
   .getElementById("file-upload")
   .addEventListener("change", handleFileSelect, false);
@@ -55,9 +56,13 @@ function handleFileSelect(event) {
   reader.readAsText(event.target.files[0]);
 }
 
-function handleFileLoad(event) {
-  console.log(event.target.result);
-  editor.setValue(event.target.result);
+function handleFileSelect(event) {
+  const reader = new FileReader();
+  reader.onload = function(e) {
+    const gcode = e.target.result;
+    evaluateJs(`_fab.loadGCode(\`${gcode}\`)`);
+  };
+  reader.readAsText(event.target.files[0]);
 }
 
 function showInfoModal() {
@@ -68,4 +73,16 @@ function showInfoModal() {
   span.onclick = function () {
     modal.style.display = "none";
   };
+}
+
+function loadGCodeFile(event) {
+  const file = event.target.files[0];
+  if (file) {
+    const reader = new FileReader();
+    reader.onload = function(e) {
+      const gcode = e.target.result;
+      fab.loadGCode(gcode); // Load the G-code into the Fab instance
+    };
+    reader.readAsText(file);
+  }
 }
